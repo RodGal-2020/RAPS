74172917098e7f0862d596f8d320646e15de1cdb
Xopre <sangrelemental@gmail.com>
2022-04-18 18:31:43 +0200

    Heading to control versions

--- R/read_xml_p_system.R
+++ R/read_xml_p_system.R
@@ -15,7 +15,7 @@ read_xml_p_system = function(demo_mode = TRUE, path = NULL, verbose = TRUE) {
   #   "dplyr",
   #   "tibble",
   #   "xml2")
-  cat("RAPS 0.02\n\n")
+  cat("Using RAPS 0.02\n\n")
 
 
   ### Aux functions
