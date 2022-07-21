5c95e650df7ee8811365f5adedce6344c1b90038
Xopre <sangrelemental@gmail.com>
2022-07-21 13:23:30 +0200

    New badges from lifecycle

--- R/alg_gillespie.R
+++ R/alg_gillespie.R
@@ -1,6 +1,6 @@
 #' Template for new functions
 #'
-#' BADGE: ![GitHub CI](https://github.com/dwyl/auth_plug/actions/workflows/ci.yml/badge.svg).
+#' `r lifecycle::badge('experimental')`
 #' @param my_param Yep, it's a parameter.
 #' @return It returns...
 #' @examples
